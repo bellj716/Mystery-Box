dbe8876b7aec7f965afef5fc80d8be07b429667c
bellj716 <51768193+bellj716@users.noreply.github.com>
2020-05-21 14:50:38 +1200

    1234567

---
+++ 01-mystery_skeletonv1.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from functools import partial   # To prevent unwanted windows
+import random
+
+
+class Start:
+    def __init__(self,parent):
+
+        # GUI to get starting balance and stakes
+        self.start_frame = Frame(padx=10, pady=10)
+        self.start_frame.grid()
+
+        #Mystery Heading (row 0)
+        self.mystery_box_label = Label(self.start_frame, text="Mystery Box Game",
+                                       font="Arial 19 bold")
+        self.mystery_box_label.grid(row=1)
+
+        # Entry box... (row 1)
+        self.start_amount_entry = Entry(self.start_frame, font="Arial 16 bold")
+        self.start_amount_entry.grid(row=1)
+
+        # Play Button (row 2)
+        self.lowstakes_button = Button(text="Low ($5)",
+                                       command=lambda: self.to_game(1))
+        self.lowstakes_button.grid(row=2, pady=10)
