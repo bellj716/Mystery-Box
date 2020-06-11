218e394d8b4989df76d57fbfc7632d58842582b0
bellj716 <51768193+bellj716@users.noreply.github.com>
2020-06-11 14:50:06 +1200

    1234

--- 04_prize_generation_V2.py
+++ 04_prize_generation_V2.py
@@ -2,31 +2,34 @@
 
 import random
 
-NUM_TRIALS = 10
+NUM_TRIALS = 100
 winnings = 0
 
 cost = NUM_TRIALS * 5
 
 for item in range(0, NUM_TRIALS):
-    prize = ""
+    #prize = ""
     round_winnings = 0
 
-    for thin in range(0, 3):
+    for thing in range(0, 3):
 
         # randint finds numbers between given end points, including both endpoints.
-        prize_num = random.randint(1,4)
-        prize += " "
+        prize_num = random.randint(1,10)
+        #prize += " "
         if prize_num == 1:
-            prize += "Gold"
+            #one in ten chance of getting gold
+            #prize += "Gold"
             round_winnings += 5
-        elif prize_num == 2:
-            prize += "Silver"
+        elif 1 < prize_num <= 3:
+            #get silver if its a 2 or a 3
+            #prize += "Silver"
             round_winnings += 2
-        elif prize_num == 3:
-            prize += "Copper"
+        elif 3 < prize_num <= 7:
+            #copper if its 4-7 40% chance
+            #prize += "Copper"
             round_winnings += 1
         else:
-            prize += "Lead"
+            #prize += "Lead"
 
     print("You won {} which is worth {}".format(prize, round_winnings))
     winnings += round_winnings
